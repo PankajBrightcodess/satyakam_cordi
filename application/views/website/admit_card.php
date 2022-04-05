eb01b3fa545f7e8c0574cdac62caf02bac9d988d
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-04-05 15:13:29 +0530

    Update admit_card.php

--- application/views/website/admit_card.php
+++ application/views/website/admit_card.php
@@ -1,6 +1,19 @@
  <section class="misvis">
       <div class="container">
         <div class="row justify-content-center">
+            <div class="col-md-9">
+            <div class="table-responsive">
+              <table class="table table-hover table-bordered" style="background:#898ab547;border:1px solid red">
+                <thead>
+                  <tr>
+                    <!-- <th scope="col"></th> -->
+                    <td scope="col"><h4 style="text-align: center;">ADMIT CARD</h4><a href="<?= base_url('/')?>" class="btn btn-sm btn-secondary" style="float: right;">Logout</a></td>
+                  </tr>
+                </thead>
+               
+              </table>
+            </div>
+          </div>
           
           <div class="col-md-9">
             <div class="table-responsive">
