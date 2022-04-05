9005a5d04a2110ad734048005b0a4f9abe9a6b9f
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-04-05 14:59:43 +0530

    Update admit_card.php

--- application/views/website/admit_card.php
+++ application/views/website/admit_card.php
@@ -7,13 +7,13 @@
               <table class="table table-hover table-bordered" style="background:#898ab547;border:1px solid red">
                 <thead>
                   <tr>
-                    <th scope="col">Name</th>
+                    <th scope="col">Name :</th>
                     <td scope="col">Pankaj Mani Tiwari</td>
                   </tr>
                 </thead>
                 <tbody>
                     <tr>
-                    <th scope="row">Father Name</th>
+                    <th scope="row">Father Name :</th>
                     <td>Sidheshwar Nath Tiwari</td>
                   </tr>
                  
@@ -22,7 +22,7 @@
                     <td>12/11/2022</td>
                   </tr>
                   <tr>
-                    <th scope="row">Admit Card</th>
+                    <th scope="row">Admit Card :</th>
                     <td><a href="<?= base_url('assets/website/images/sample.pdf')?>" class="btn btn-sm btn-success">Download</a> </td>
                   </tr>
                    
