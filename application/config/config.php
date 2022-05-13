98c8dca887de055f2dbe08cd230a0df251ce2e93
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-05-13 16:24:45 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -26,10 +26,13 @@ date_default_timezone_set('Asia/Kolkata');
 */
 if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']=='localhost'){
 	$config['base_url'] = "http://$_SERVER[HTTP_HOST]/satyakam_cordi/";
+}else{
+	$config['base_url'] = "http://indiasatyakam.com/";
+	
 }
-else{
-	$config['base_url'] = "https://demo.softwarebss.com/satyakama/";
-}
+// else{
+// 	$config['base_url'] = "https://demo.softwarebss.com/satyakama/";
+// }
 
 /*
 |--------------------------------------------------------------------------
