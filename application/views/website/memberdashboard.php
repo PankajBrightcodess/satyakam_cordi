0f4ebcfa3c4e74426bf46dc1c4b766b263e05a23
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-05-30 16:15:50 +0530

    Update memberdashboard.php

--- application/views/website/memberdashboard.php
+++ application/views/website/memberdashboard.php
@@ -1,10 +1,26 @@
- <section class="officialservices">
+ <section class="officer_dashboard">
       <div class="container">
         <div class="row justify-content-center">
-          <div class="col-md-12">
+          <div class="col-md-12 mb-5">
             <h1 class="tittle">Dashboard</h1>
-            <div class="table-responsive">
+           
+          </div>
+          <div class="col-md-12 text-center">
+            <div class="row">
+              <div class="col-md-3"><a href="<?php echo base_url('website/econtractdocx');?>" class=" btn buttondesign">e-Contract<br>(My Document)  </a></div>
+              <div class="col-md-3"><a href="<?php echo base_url('website/myofficereport');?>" class="buttondesign btn">My Office<br>(My Report)  </a></div>
+              <div class="col-md-3"><a href="<?php echo base_url('website/econtractkyc');?>" class="buttondesign btn">e-Contract<br>(KYC)  </a></div>
+              <div class="col-md-3 mb-5"><a href="<?php echo base_url('website/myofficeexpense');?>" class="buttondesign btn">My Office<br>(Office Expense)  </a></div>
+              <div class="col-md-3"><a href="<?php echo base_url('website/econtractmyteam');?>" class="buttondesign btn">My Office<br>(My Team Office)  </a></div>
+              <div class="col-md-3"><a href="<?php echo base_url('website/ajeevikageneralgrp');?>" class="buttondesign btn">General<br>Group</a></div>
+              <div class="col-md-3"> <a href="<?php echo base_url('website/vacency_report')?>" class="buttondesign btn">Vacancy <br>List </a></div>
+              <div class="col-md-3 mb-5"><a href="#" class="buttondesign btn">Events<br> List </a></div>
+              <div class="col-md-3"></div>
+              <div class="col-md-3"><a href="#" class="buttondesign btn">Gallery<br> List </a></div>
+              <div class="col-md-3"><a href="#" class="buttondesign btn">I-CON India   </a></div>
               
+              <div class="col-md-3"></div>
+             
             </div>
           </div>
            
