1a4fba84aeddc00c1e74b75ab71cacf683502007
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-05-30 15:27:07 +0530

    update

--- application/views/website/officer_dashboard.php
+++ application/views/website/officer_dashboard.php
@@ -1,10 +1,26 @@
- <section class="officialservices">
+ <section class="officer_dashboard">
       <div class="container">
         <div class="row justify-content-center">
-          <div class="col-md-12">
+          <div class="col-md-12 mb-5">
             <h1 class="tittle">Dashboard</h1>
-            <div class="table-responsive">
+           
+          </div>
+          <div class="col-md-12 text-center">
+            <div class="row">
+              <div class="col-md-3"><button class="buttondesign">e-Contract<br>(My Document)  </button></div>
+              <div class="col-md-3"><button class="buttondesign">My Office<br>(My Report)  </button></div>
+              <div class="col-md-3"><button class="buttondesign">e-Contract<br>(KYC)  </button></div>
+              <div class="col-md-3 mb-5"><button class="buttondesign">My Office<br>(Office Expense)  </button></div>
+              <div class="col-md-3"><button class="buttondesign">My Office<br>(My Team Office)  </button></div>
+              <div class="col-md-3"><button class="buttondesign">General<br>Group</button></div>
+              <div class="col-md-3"><button class="buttondesign">Vacancy <br>List </button></div>
+              <div class="col-md-3 mb-5"><button class="buttondesign">Events<br> List </button></div>
+              <div class="col-md-3"></div>
+              <div class="col-md-3"><button class="buttondesign">Gallery<br> List </button></div>
+              <div class="col-md-3"><button class="buttondesign">Vacancy  </button></div>
               
+              <div class="col-md-3"></div>
+             
             </div>
           </div>
            
