658e5e8d1780367ef05b374dd82de0dbf308ca93
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-06-15 18:30:10 +0530

    Update Website_model.php

--- application/models/Website_model.php
+++ application/models/Website_model.php
@@ -48,7 +48,7 @@ class Website_model extends CI_Model{
 		unset($data['app_date']);
 		unset($data['state_id']);
 		$status=$this->db->insert($table,$data);
-		return  $this->db->last_query();
+		echo print_r($status);
 		die;
 		if($status){
 			return true;
