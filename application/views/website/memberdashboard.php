c2493f36ec14fa127b1d38a1e26ab2c1dfd67fea
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-07-19 12:50:11 +0530

    Update memberdashboard.php

--- application/views/website/memberdashboard.php
+++ application/views/website/memberdashboard.php
@@ -7,14 +7,14 @@
           </div>
           <div class="col-md-12 text-center">
             <div class="row">
-              <div class="col-md-3"><a href="<?php echo base_url('website/my_group');?>" class=" btn buttondesign">My Profile<br> </a></div>
-              <div class="col-md-3"><a href="<?php echo base_url('website/member_general_group');?>" class="buttondesign btn">my General<br>Group  </a></div>
-              <div class="col-md-3"><a href="<?php echo base_url('website/my_club_group_features');?>" class="buttondesign btn">My Club<br>Group </a></div>
-              <div class="col-md-3 mb-5"><a href="<?php echo base_url('website/my_saving_account_features');?>" class="buttondesign btn">Saving<br>Account</a></div>
-              <div class="col-md-3"><a href="<?php echo base_url('website/my_loan_account_features');?>" class="buttondesign btn">My Loan<br>Account  </a></div>
-              <div class="col-md-3"><a href="<?php echo base_url('website/my_loan_account_features');?>" class="buttondesign btn">Notification</a></div>
-              <div class="col-md-3"> <a href="<?php echo base_url('website/create_account_page')?>" class="buttondesign btn">Create <br>Account </a></div>
-              <div class="col-md-3 mb-5"><a href="#" class="buttondesign btn">Account<br> Status </a></div>
+              <div class="col-md-3 col-6 mb-2"><a href="<?php echo base_url('website/my_group');?>" class=" btn buttondesign">My Profile<br> </a></div>
+              <div class="col-md-3 col-6 mb-2"><a href="<?php echo base_url('website/member_general_group');?>" class="buttondesign btn">my General<br>Group  </a></div>
+              <div class="col-md-3 col-6 mb-2"><a href="<?php echo base_url('website/my_club_group_features');?>" class="buttondesign btn">My Club<br>Group </a></div>
+              <div class="col-md-3 col-6 mb-2"><a href="<?php echo base_url('website/my_saving_account_features');?>" class="buttondesign btn">Saving<br>Account</a></div>
+              <div class="col-md-3 col-6 mb-2"><a href="<?php echo base_url('website/my_loan_account_features');?>" class="buttondesign btn">My Loan<br>Account  </a></div>
+              <div class="col-md-3 col-6 mb-2"><a href="<?php echo base_url('website/my_loan_account_features');?>" class="buttondesign btn">Notification</a></div>
+              <div class="col-md-3 col-6 mb-2"> <a href="<?php echo base_url('website/create_account_page')?>" class="buttondesign btn">Create <br>Account </a></div>
+              <div class="col-md-3 col-6 mb-2"><a href="#" class="buttondesign btn">Account<br> Status </a></div>
              <!--  <div class="col-md-3"></div>
               <div class="col-md-3"><a href="#" class="buttondesign btn">Gallery<br> List </a></div>
               <div class="col-md-3"><a href="#" class="buttondesign btn">I-CON India   </a></div>
