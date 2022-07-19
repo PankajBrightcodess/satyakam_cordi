86f6a7d80e332df877089e5bb1dab2025d5f487b
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-07-19 11:54:05 +0530

    Update officer_dashboard.php

--- application/views/website/officer_dashboard.php
+++ application/views/website/officer_dashboard.php
@@ -7,19 +7,19 @@
           </div>
           <div class="col-md-12 text-center">
             <div class="row">
-              <div class="col-md-3 col-6 "><a href="<?php echo base_url('website/econtractdocx');?>" class=" btn buttondesign">e-Contract<br>(My Document)  </a></div>
-              <div class="col-md-3 col-6"><a href="<?php echo base_url('website/myofficereport');?>" class="buttondesign btn">My Office<br>(My Report)  </a></div>
-              <div class="col-md-3 col-6"><a href="<?php echo base_url('website/econtractkyc');?>" class="buttondesign btn">e-Contract<br>(KYC)  </a></div>
-              <div class="col-md-3 col-6 "><a href="<?php echo base_url('website/myofficeexpense');?>" class="buttondesign btn">My Office<br>(Office Expense)  </a></div>
-              <div class="col-md-3 col-6"><a href="<?php echo base_url('website/econtractmyteam');?>" class="buttondesign btn">My Office<br>(My Team Office)  </a></div>
-              <div class="col-md-3 col-6"><a href="<?php echo base_url('website/ajeevikageneralgrp');?>" class="buttondesign btn">General<br>Group</a></div>
+              <div class="col-md-3 col-6 mb-2"><a href="<?php echo base_url('website/econtractdocx');?>" class=" btn buttondesign">e-Contract<br>(My Document)  </a></div>
+              <div class="col-md-3 col-6 mb-2"><a href="<?php echo base_url('website/myofficereport');?>" class="buttondesign btn">My Office<br>(My Report)  </a></div>
+              <div class="col-md-3 col-6 mb-2"><a href="<?php echo base_url('website/econtractkyc');?>" class="buttondesign btn">e-Contract<br>(KYC)  </a></div>
+              <div class="col-md-3 col-6 mb-2"><a href="<?php echo base_url('website/myofficeexpense');?>" class="buttondesign btn">My Office<br>(Office Expense)  </a></div>
+              <div class="col-md-3 col-6 mb-2"><a href="<?php echo base_url('website/econtractmyteam');?>" class="buttondesign btn">My Office<br>(My Team Office)  </a></div>
+              <div class="col-md-3 col-6 mb-2"><a href="<?php echo base_url('website/ajeevikageneralgrp');?>" class="buttondesign btn">General<br>Group</a></div>
               <div class="col-md-3"> <a href="<?php echo base_url('website/vacency_report')?>" class="buttondesign btn">Vacancy <br>List </a></div>
-              <div class="col-md-3 col-6 "><a href="#" class="buttondesign btn">Events<br> List </a></div>
+              <div class="col-md-3 col-6 mb-2 "><a href="#" class="buttondesign btn">Events<br> List </a></div>
               <div class="col-md-3 "></div>
-              <div class="col-md-3 col-6"><a href="#" class="buttondesign btn">Gallery<br> List </a></div>
-              <div class="col-md-3 col-6"><a href="#" class="buttondesign btn">I-CON India   </a></div>
+              <div class="col-md-3 col-6 mb-2"><a href="#" class="buttondesign btn">Gallery<br> List </a></div>
+              <div class="col-md-3 col-6 mb-5"><a href="#" class="buttondesign btn">I-CON India   </a></div>
               
-              <div class="col-md-3"></div>
+              <div class="col-md-3 "></div>
              
             </div>
           </div>
