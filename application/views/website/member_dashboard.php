045fc3c6c013a8682ccf4011dfc0c11394b56f24
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-07-19 16:36:17 +0530

    update

--- application/views/website/member_dashboard.php
+++ application/views/website/member_dashboard.php
@@ -14,15 +14,25 @@
                 <tbody>
                   <tr>
                     <td><label class="ml-2">MEMBERSHIP CERTIFICATE</label> <a href="<?= base_url('website/get_member_certificate');?>" class="btn btn-sm btn-success mr-5" >DOWNLOAD</a></td>
-                    <td>AADHAR <a href="<?= base_url($uploadfiles['aadhar_front']);?>" class="btn btn-sm btn-success ml-2">DOWNLOAD</a></td>
+                     <td>QUALIFICATION CERTIFICATE <a href="<?= base_url($uploadfiles['qualification_certificate']);?>" class="btn btn-sm btn-success ml-2" >DOWNLOAD</a></td>
+                    
+                  </tr>
+                  <tr>  
+                    <td>AADHAR(Front) <a href="<?= base_url($uploadfiles['aadhar_front']);?>" class="btn btn-sm btn-success ml-2">DOWNLOAD</a></td>
+                    <td>AADHAR(back) <a href="<?= base_url($uploadfiles['aadhar_back']);?>" class="btn btn-sm btn-success ml-2">DOWNLOAD</a></td>
+                  </tr>
+                  <tr>  
+                   <td>ELECTION ID CARD(Front)<a href="<?= base_url($uploadfiles['election_id_card']);?>" class="btn btn-sm btn-success ml-2" >DOWNLOAD</a></td>
+                    <td>ELECTION ID CARD(Back)<a href="<?= base_url($uploadfiles['election_id_card_back']);?>" class="btn btn-sm btn-success ml-2">DOWNLOAD</a></td>
                   </tr>
                   <tr>
-                    <td>ELECTION ID CARD<a href="<?= base_url($uploadfiles['election_id_card']);?>" class="btn btn-sm btn-success ml-2" >DOWNLOAD</a></td>
+                    
                     <td>PAN CARD <a href="<?= base_url($uploadfiles['pan_card']);?>" class="btn btn-sm btn-success ml-2" >DOWNLOAD</a></td>
+                     <td ><label class="ml-5">PASSBOOK</label> <a href="<?= base_url($uploadfiles['passbook_bank']);?>" class="btn btn-sm btn-success ml-2" >DOWNLOAD</a></td>
                   </tr>
                   <tr>
-                    <td ><label class="ml-5">PASSBOOK</label> <a href="<?= base_url($uploadfiles['passbook_bank']);?>" class="btn btn-sm btn-success ml-2" >DOWNLOAD</a></td>
-                    <td>CERTIFICATE <a href="<?= base_url($uploadfiles['qualification_certificate']);?>" class="btn btn-sm btn-success ml-2" >DOWNLOAD</a></td>
+                   
+                   
                   </tr>
                   <tr>
                     <td ><img src="<?= base_url($uploadfiles['member_sign']);?>" class="img-fluid" height="200" width="300" ></td>
