b982f2d4d7b3a2002008f8a9025f3e354c664c46
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-07-28 16:00:32 +0530

    update

--- application/views/website/privacypolicy.php
+++ application/views/website/privacypolicy.php
@@ -1,7 +1,7 @@
 <section class="privacypolicy">
       <div class="container">
         <div class="row">
-         <!--  <div class="col-md-12">
+           <div class="col-md-12">
             <h2>Privacy Policy</h2>
             <p>Your privacy is important to us. It is Satyakam Foundation Trust's policy to respect your privacy regarding any information we may collect from you across our website, Satyakam Foundation Trust , and other sites we own and operate.We only ask for personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we're collecting it and how it will be used. We only retain collected information for as long as necessary to provide you with your requested service. What data we store, we'll protect within commercially acceptable means to prevent loss and theft, as well as unauthorized access, disclosure, copying, use or modification. We don't share any personally identifying information publicly or with third-parties, except when required to by law. Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies. You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services. Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us.</p>
             <h2>Terms</h2>
@@ -31,11 +31,11 @@
             <p>Satyakam Foundation Trust may revise these terms of service for its website at any time without notice. By using this website you are agreeing to be bound by the then current version of these terms of service.</p>
             <h2>Governing Law</h2>
             <p>These terms and conditions are governed by and construed in accordance with the laws of Satyakam Foundation Trust and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.</p>
-          </div> -->
-          <div class="col-md-12">
+          </div> 
+         <!--  <div class="col-md-12">
             <h2>Privacy Policy</h2>
             <p>Your privacy is important to us.We respect your privacy !regarding any information we may collect from you across our website, <a href="https://indiasatyakam.com">https://indiasatyakam.com</a> and other sites we own and operate.</p>
-          </div>
+          </div> -->
         </div>
       </div>
     </section>
