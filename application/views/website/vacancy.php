19bdeef0d7f596725c241917710803810b3d6362
Pankaj Tiwari <86092953+PankajBrightcodess@users.noreply.github.com>
2022-08-16 18:27:56 +0530

    update

--- application/views/website/vacancy.php
+++ application/views/website/vacancy.php
@@ -1,27 +1,37 @@
-    <section class="vncy">
-      <div class="container">
+   <section class="vncy">
+      <div class="container-fluid">
         <div class="row justify-content-center">
-          <div class="col-md-12">
-            <h4 class="tittle">VACANCY</h4>
+          <div class="col-md-6">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active"><img src="<?= base_url('assets/website/images/bannerimage.jpg')?>" style="border-radius: 10px;" class="d-block w-100" alt="satyakam banner 2"></div>
+              </div>
+            </div>
           </div>
-          <div class="col-md-3">
-             <a class="dropdown-item" href="<?= base_url('website/mainvacency');?>" style="color:white"><h6 class="tittle">Vacancy</h6></a>
+          <div class="col-md-6">
+          <div class="col-md-12 ">
+            <h4  style="font-weight: 900;font-size: 50px;text-align: center; color: white;">VACANCY</h4>
+          </div> 
+          <div class="col-md-12 col-12 ">
+             <a class="dropdown-item" href="<?= base_url('website/mainvacency');?>" style="color:white; text-align: center;"><h6 class="tittle">Vacancy</h6></a>
           </div>
-         <!--  <div class="col-md-3">
-            <a class="dropdown-item" target="_blank" href="<?= base_url('assets/website/legaldocpdf/sgdh.pdf')?>" style="color:white"><h6 class="tittle">Guideline</h6></a> 
-          </div> -->
-          <div class="col-md-3">
+ 
+          <div class="col-md-12 col-12 ">
             <a class="dropdown-item" href="<?= base_url('website/vacency_login');?>" style="color:white"><h6 class="tittle">Online Apply</h6></a>
           </div>
-          <div class="col-md-3">
+          
+          <div class="col-md-12 col-12 ">
             <a class="dropdown-item" href="<?= base_url('website/vacency_login_foradmitcard');?>" style="color:white;"><h6 class="tittle">Download Admit Card</h6></a>
           </div>
-          <div class="col-md-3">
+         
+          <div class="col-md-12 col-12 ">
             <a class="dropdown-item" href="<?= base_url('website/vacency_login_forresult');?>" style="color:white"><h6 class="tittle">Results</h6></a>
           </div>
-           
-          <div class="col-md-3">
+         
+          <div class="col-md-12 col-12 ">
             <a class="dropdown-item" href="<?= base_url('website/helpline');?>" style="color:white"><h6 class="tittle">Helpline</h6></a>
+          </div>
+                  </div>
           </div>
           
         </div>
