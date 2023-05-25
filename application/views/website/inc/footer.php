54d503651f669829f8a074ff4e6b30f4a701fbeb
satya-office <Satyaki.bhattacharya@brightcodess.com>
2023-05-25 16:32:21 +0530

    sai

--- application/views/website/inc/footer.php
+++ application/views/website/inc/footer.php
@@ -1,38 +1,65 @@
 <div class="footer">
-    <div class="container-fluid">
-        
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="footer-newsletter">
-                        <h2 style="border-bottom: 1px solid white;">Satyakam Foundation Trust</h2>
-                        <p style="text-align:justify">The operation of all the ambitious schemes run by the Satyakam Foundation Trust , mainly to provide many opportunities in the field of health and education and prosperity of rural India, our organization is dedicated to providing help to all sections and communities of the society through its innovative schemes.</p>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="footer-link " style="font-size:13px">
-                        <h2 style="border-bottom: 1px solid white;">Quick Links</h2>
-                        <!-- <a href="#">Home</a>
+  <div class="container-fluid">
+
+    <div class="row">
+      <div class="col-md-4">
+        <div class="footer-newsletter">
+          <h2 style="border-bottom: 1px solid white;">Satyakam Foundation Trust</h2>
+          <p style="text-align:justify">The operation of all the ambitious schemes run by the Satyakam Foundation Trust , mainly to provide many opportunities in the field of health and education and prosperity of rural India, our organization is dedicated to providing help to all sections and communities of the society through its innovative schemes.</p>
+        </div>
+      </div>
+      <div class="col-md-2">
+        <div class="footer-link " style="font-size:13px">
+          <h2 style="border-bottom: 1px solid white;">Quick Links</h2>
+          <!-- <a href="#">Home</a>
                         <a href="#">Services</a>
                         <a href="#">Gallery</a>
                         <a href="#">About Us</a>
                         <a href="#">Contact Us</a> -->
-                       <a href="<?php echo base_url('website/term_condition');?>">Terms and Condition</a>
-                        <a href="<?php echo base_url('website/privacypolicy');?>">Privacy Policy</a>
-                        <a href="<?php echo base_url('website/changepolicy');?>">Return and Refund Policy</a>
-                        <a href="<?php echo base_url('website/cancel_policy');?>">Cancellation Policy</a>
+          <a href="<?php echo base_url('website/term_condition'); ?>">Terms and Condition</a>
+          <a href="<?php echo base_url('website/privacypolicy'); ?>">Privacy Policy</a>
+          <a href="<?php echo base_url('website/changepolicy'); ?>">Return and Refund Policy</a>
+          <a href="<?php echo base_url('website/cancel_policy'); ?>">Cancellation Policy</a>
 
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="footer-contact">
-                        <h2 style="border-bottom: 1px solid white;">Our Address</h2>
-                        <p><i class="fa fa-map-marker"></i>Maal Godown Road, Near Railway Station, Ward No.-18, Madhubani (Bihar) - 847211</p>
-                        <p><i class="fa fa-phone"></i>+91-9031023059, 9031023060</p>
-                        <p><i class="fa fa-envelope"></i>indiaskfoundation468@gmail.com</p>
-                    </div>
-                </div>
-                <div class="col-md-2 footer-newsletter">
-                    <h2 style="border-bottom: 1px solid white;">Social Links</h2>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="footer-contact">
+          <h2 style="border-bottom: 1px solid white;">Our Address</h2>
+          <p><i class="fa fa-map-marker"></i>Maal Godown Road, Near Railway Station, Ward No.-18, Madhubani (Bihar) - 847211</p>
+          <p><i class="fa fa-phone"></i>+91-9031023059, 9031023060</p>
+          <p><i class="fa fa-envelope"></i>indiaskfoundation468@gmail.com</p>
+        </div>
+      </div>
+      <div class="col-md-2 footer-newsletter">
+        <div class="row">
+          <div class="col-md-6 col-3">
+            <a href="<?= base_url('assets/website/images/logo1Copy-removebg-preview.png') ?>" data-lightbox="image-1" data-title="" style="color:black;">
+              <img src="<?= base_url('assets/website/images/logo1Copy-removebg-preview.png') ?>" alt="gallery1" width="100%">
+            </a>
+            <!-- <img src="<?= base_url('assets/website/images/logo1Copy-removebg-preview.png') ?>" width="100%"> -->
+          </div>
+          <div class="col-md-6 col-3">
+            <a href="<?= base_url('assets/website/images/l2.png') ?>" data-lightbox="image-1" data-title="" style="color:black;">
+              <img src="<?= base_url('assets/website/images/l2.png') ?>" alt="gallery1" width="100%">
+            </a>
+            <!-- <img src="<?= base_url('assets/website/images/l2.png') ?>" width="100%"> -->
+          </div>
+
+          <div class="col-md-6 col-3">
+            <a href="<?= base_url('assets/website/images/l3.png') ?>" data-lightbox="image-1" data-title="" style="color:black;">
+              <img src="<?= base_url('assets/website/images/l3.png') ?>" alt="gallery1" width="100%">
+            </a>
+            <!-- <img src="<?= base_url('assets/website/images/l3.png') ?>" width="100%"> -->
+          </div>
+          <div class="col-md-6 col-3">
+            <a href="<?= base_url('assets/website/images/l4.png') ?>" data-lightbox="image-1" data-title="" style="color:black;">
+              <img src="<?= base_url('assets/website/images/l4.png') ?>" alt="gallery1" width="100%">
+            </a>
+            <!-- <img src="<?= base_url('assets/website/images/l4.png') ?>" width="100%"> -->
+          </div>
+        </div>
+        <!-- <h2 style="border-bottom: 1px solid white;">Social Links</h2>
                     <div class="footer-social" style="margin-bottom: 10px;margin-left: 26px;">
                         <a href="https://twitter.com/Indiasatyakam22" target="blank"><i class="fab fa-twitter"></i></a>
                         <a href="https://www.facebook.com/Indiasatyakamfoundationtrust" target="blank"><i class="fab fa-facebook-f" target="blank"></i></a>
@@ -43,27 +70,28 @@
                     <div class="form">
                         <input class="form-control" placeholder="Email goes here">
                         <button class="btn custom-btn" style="background:#ff6c16;color: black;">Submit</button>
-                    </div>
-                </div>
-                
-                
-            </div>
-        
+                    </div> -->
+
+      </div>
+
+
     </div>
-    <div class="copyright" style="border-top: 1px dashed #f8ff00;">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-6">
-                    <p>All Right Reserved,2022 <a href="#" style="color:#fff"> Satyakam Foundation Trust</a></p>        
-                </div>
-                <div class="col-md-6">
-                    <p>Designed By <a href="https://www.brightcodess.com/" style="color:#fff"> Brightcode Software Services Pvt Ltd.</a></p>        
-                </div>
-            </div>
-            
-            
+
+  </div>
+  <div class="copyright" style="border-top: 1px dashed #f8ff00;">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6">
+          <p>All Right Reserved,2022 <a href="#" style="color:#fff"> Satyakam Foundation Trust</a></p>
         </div>
+        <div class="col-md-6">
+          <p>Designed By <a href="https://www.brightcodess.com/" style="color:#fff"> Brightcode Software Services Pvt Ltd.</a></p>
+        </div>
+      </div>
+
+
     </div>
+  </div>
 </div>
 <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
 
@@ -72,22 +100,22 @@
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
-        <form method="POST" action="<?php echo base_url('website/login_vacency_foradmitcard')?>">
-          <div class="modal-header" style="background:#2e3192; color: white;">
-            <h5 class="modal-title" id="exampleModalCenterTitle">Login</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
+      <form method="POST" action="<?php echo base_url('website/login_vacency_foradmitcard') ?>">
+        <div class="modal-header" style="background:#2e3192; color: white;">
+          <h5 class="modal-title" id="exampleModalCenterTitle">Login</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-md-12 col-12 mb-3"><input type="email" name="emailid" placeholder="Enter Valid Email" class="form-control"></div>
+            <div class="col-md-12 col-12"><input type="password" name="password" placeholder="Enter Password" class="form-control"></div>
           </div>
-          <div class="modal-body">
-           <div class="row">
-               <div class="col-md-12 col-12 mb-3"><input type="email" name="emailid" placeholder="Enter Valid Email" class="form-control"></div>
-               <div class="col-md-12 col-12"><input type="password" name="password" placeholder="Enter Password" class="form-control"></div>
-           </div>
-          </div>
-          <div class="modal-footer">
-            <button type="submit" class="btn btn-info">Submit</button>
-          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-info">Submit</button>
+        </div>
       </form>
     </div>
   </div>
